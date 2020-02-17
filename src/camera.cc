e663daa49a38b1746a837b7ecd08dd3a67a3039f
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-02-17 17:28:17 +0100

    Add getters for Camera attributes

--- src/camera.cc
+++ src/camera.cc
@@ -3,3 +3,23 @@
 //
 
 #include "camera.hh"
+
+Point3 Camera::origin() const {
+    return org_;
+}
+
+Point3 Camera::target() const {
+    return target_;
+}
+
+float Camera::fov_x() const {
+    return fov_x_;
+}
+
+float Camera::fov_y() const {
+    return fov_y_;
+}
+
+float Camera::z_min() const {
+    return z_min_;
+}
