ff35a168df351ddef5097835605f4cac303235e1
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-02-17 17:32:57 +0100

    Add gen_img method in Scene

--- src/scene.cc
+++ src/scene.cc
@@ -6,18 +6,33 @@
 
 #include <iostream>
 
-Color Scene::cast_ray(const Vector3 &dir) {
+Color Scene::cast_ray(const Ray &r) const {
     for(auto &o: objects_)
     {
-        if (o.intersects({{0, 0, 0}, dir}) != -1)
+        if (o->intersects(r) != -1)
         {
-            auto [kd, kl, color] = o.get_texture_elms({0, 0, 0});
+            std::cout << r << std::endl;
+            auto [kd, kl, color] = o->get_texture_elms({0, 0, 0});
             return color;
         }
     }
     return Color(0, 0, 0);
 }
 
-void Scene::add_object(Sphere o) {
+void Scene::add_object(Object *o) {
     objects_.push_back(o);
+}
+
+Image Scene::gen_img() const {
+    Image im(img_width_, img_height_);
+    for (int i = 0; i < img_height_; i++)
+    {
+        for (int j = 0; j < img_width_; j++)
+        {
+            Vector3 dir = tl_ + (i / screen_height_) * Vector3::down() + (j / screen_width_) * Vector3::right();
+            Color color = cast_ray({cam_.origin(), dir});
+            im.set_pixel(i, j, color);
+        }
+    }
+    return im;
 }
--- src/scene.hh
+++ src/scene.hh
@@ -4,23 +4,43 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 
 #include "object.hh"
 #include "light.hh"
 #include "camera.hh"
+#include "image.hh"
 
 class Scene {
 public:
-    Scene(const Camera &c): cam_(c) {}
-    Color cast_ray(const Vector3 &dir);
+    Scene(const Camera &c, int img_width, int img_height):
+    cam_(c),
+    img_width_(img_width),
+    img_height_(img_height)
+    {
+        z_min_ = c.z_min();
 
-    void add_object(Sphere o);
+        screen_width_ =  2 * c.z_min() * tanf(c.fov_x());
+        screen_height_ = 2 * c.z_min() * tanf(c.fov_y());
+        screen_center_ = c.origin() + ((c.target() - c.origin()).normalize() * c.z_min());
+
+        tl_ = screen_center_ + (screen_width_ / 2) * Vector3::left() + (screen_height_ / 2) * Vector3::up();
+    }
+    Color cast_ray(const Ray &r) const;
+
+    Image gen_img() const;
+
+    void add_object(Object *o);
 
 private:
-    std::vector<Sphere> objects_;
+    std::vector<Object *> objects_;
     std::vector<Light> lights_;
 
     Camera cam_;
-    float z_min_;
+    Vector3 screen_center_, tl_;
+    float screen_width_,
+        screen_height_,
+        z_min_;
+    int img_width_, img_height_;
 };
 
