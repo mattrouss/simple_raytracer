308759f05b39b9b6b7e8c82fd25bdf045f46b49d
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-02-17 21:45:45 +0100

    Replace Point3 with Vector3

--- src/math_types.cc
+++ src/math_types.cc
@@ -1,66 +1,27 @@
 #include "math_types.hh"
 
 
-
 std::ostream &operator<<(std::ostream &out, Color &c) {
     return out << "Color(r=" << unsigned(c.r) << ", g=" << unsigned(c.g) << ", b=" << unsigned(c.b) << ")";
 }
 
-
-Vector3 Point3::operator+(const Point3 &p) const {
-    return {
-        x_ + p.x_,
-        y_ + p.y_,
-        z_ + p.z_
-    };
+float Color::r_intensity() const {
+    return (float) r / 255.f;
 }
 
-Vector3 Point3::operator+(const Vector3 &v) const {
-    return {
-            x_ + v.x(),
-            y_ + v.y(),
-            z_ + v.z()
-    };
+float Color::g_intensity() const {
+    return (float) g / 255.f;
 }
 
-Vector3 Point3::operator-(const Point3 &p) const {
-    return {
-            x_ - p.x_,
-            y_ - p.y_,
-            z_ - p.z_
-    };
+float Color::b_intensity() const {
+    return (float) b / 255.f;
 }
-
-float Point3::x() const {
-    return x_;
-}
-
-float Point3::y() const {
-    return y_;
-}
-
-float Point3::z() const {
-    return z_;
-}
-
-std::ostream& operator<<(std::ostream &out, const Point3 &p) {
-    return out << "Point3(" << p.x_ << ", " << p.y_ << ", " << p.z_ << ")";
-}
-
 
 Vector3 Vector3::operator+(const Vector3 &v) const {
     return {
             x_ + v.x_,
             y_ + v.y_,
             z_ + v.z_
-    };
-}
-
-Vector3 Vector3::operator+(const Point3 &p) const {
-    return {
-            x_ + p.x(),
-            y_ + p.y(),
-            z_ + p.z()
     };
 }
 
@@ -128,7 +89,7 @@ Vector3 Vector3::cross(const Vector3 &v) const {
     };
 }
 
-Vector3 Vector3::normalize() const {
+Vector3 Vector3::normalized() const {
     Vector3 copy = *this;
     copy = copy / magnitude();
     return copy;
@@ -148,11 +109,11 @@ Vector3 Vector3::down() {
 }
 
 Vector3 Vector3::left() {
-    return Vector3(-1, 0, 0);
+    return Vector3(1, 0, 0);
 }
 
 Vector3 Vector3::right() {
-    return Vector3(1, 0, 0);
+    return Vector3(-1, 0, 0);
 }
 
 Vector3 Vector3::forward() {
@@ -168,4 +129,8 @@ std::ostream &operator<<(std::ostream &out, const Ray &r) {
     out << "Ray (Origin: " << r.org;
     out << ", Direction: " << r.dir << ")" << std::endl;
     return out;
+}
+
+Vector3 Ray::point_at_parameter(float t) const {
+    return org + t * dir;
 }
--- src/math_types.hh
+++ src/math_types.hh
@@ -4,13 +4,17 @@
 #include <cstdint>
 #include <cmath>
 
-class Point3;
+class Vector3;
 
 struct Color
 {
     uint8_t r, g, b;
 
-    Color(uint8_t r_ = 0, uint8_t g_ = 0, uint8_t b_ = 0): r(r_), g(g_), b(b_) {}
+    explicit Color(uint8_t r_ = 0, uint8_t g_ = 0, uint8_t b_ = 0): r(r_), g(g_), b(b_) {}
+
+    float r_intensity() const;
+    float g_intensity() const;
+    float b_intensity() const;
 
     friend std::ostream& operator<<(std::ostream &out, Color &c);
 };
@@ -21,7 +25,6 @@ public:
     Vector3(float x = 0, float y = 0, float z = 0): x_(x), y_(y), z_(z) {}
 
     Vector3 operator+(const Vector3 &v) const;
-    Vector3 operator+(const Point3 &p) const;
     Vector3 operator-(const Vector3 &v) const;
     Vector3 operator*(const float &l) const;
     friend Vector3 operator*(const float &l, const Vector3 &v);
@@ -44,26 +47,7 @@ public:
     float sqr_magnitude() const;
     float magnitude() const;
     Vector3 cross(const Vector3 &v) const;
-    Vector3 normalize() const;
-
-private:
-    float x_, y_, z_;
-};
-
-class Point3
-{
-public:
-    Point3(float x, float y, float z): x_(x), y_(y), z_(z) {}
-
-    Vector3 operator+(const Point3 &p) const;
-    Vector3 operator+(const Vector3 &v) const;
-    Vector3 operator-(const Point3 &p) const;
-
-    float x() const;
-    float y() const;
-    float z() const;
-
-    friend std::ostream& operator<<(std::ostream &out, const Point3 &p);
+    Vector3 normalized() const;
 
 private:
     float x_, y_, z_;
@@ -71,10 +55,12 @@ private:
 
 struct Ray
 {
-    Point3 org;
+    Vector3 org;
     Vector3 dir;
 
-    Ray(const Point3 &org_, const Vector3 &dir_): org(org_), dir(dir_) {}
+    Ray(const Vector3 &org_, const Vector3 &dir_): org(org_), dir(dir_) {}
+
+    Vector3 point_at_parameter(float t) const;
 
     friend std::ostream& operator<<(std::ostream &out, const Ray &r);
 };
