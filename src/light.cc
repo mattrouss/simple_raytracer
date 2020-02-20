e76770a38ada0ade2b418cfd6b98ab0538b51a79
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-02-20 16:43:59 +0100

    Add Light_Intensity struct

--- src/light.cc
+++ src/light.cc
@@ -4,10 +4,40 @@
 
 #include "light.hh"
 
+Color Light_Intensity::to_rgb() const {
+    return {
+            (uint8_t)std::clamp<float>(r * 255, 0, 255),
+            (uint8_t)std::clamp<float>(g * 255, 0, 255),
+            (uint8_t)std::clamp<float>(b * 255, 0, 255),
+    };
+}
+
+Light_Intensity Light_Intensity::operator+(const Light_Intensity &l) {
+    return {
+            r + l.r,
+            g + l.g,
+            b + l.b
+    };
+}
+
+Light_Intensity Light_Intensity::operator+=(const Light_Intensity &l) {
+    *this = *this + l;
+    return *this;
+}
+
+
+std::ostream &operator<<(std::ostream &out, Light_Intensity &l) {
+    return out << "Light_Intensity(r=" << l.r << ", g=" << l.g << ", b=" << l.b << ")";
+}
+
 Light_Intensity Light::intensity() const {
     return intensity_;
 }
 
 Vector3 Point_Light::direction_from(const Vector3 &v) const {
     return (org_ - v).normalized();
+}
+
+Vector3 Point_Light::position() const {
+    return org_;
 }
--- src/light.hh
+++ src/light.hh
@@ -4,24 +4,43 @@
 #pragma once
 
 #include "math_types.hh"
+#include "object.hh"
+
+class Light;
+
+struct Light_Intensity
+{
+    float r, g, b;
+
+    Light_Intensity(float r_ = 0, float g_ = 0, float b_ = 0): r(r_), g(g_), b(b_) {}
+
+    Light_Intensity operator+(const Light_Intensity &l);
+    Light_Intensity operator+=(const Light_Intensity &l);
+
+    Color to_rgb() const;
+
+    friend std::ostream& operator<<(std::ostream &out, Light_Intensity &l);
+};
 
 class Light {
 public:
-    explicit Light(const Light_Intensity &intensity): intensity_(intensity) {}
+    explicit Light(const Light_Intensity &intensity, const Vector3 &org): intensity_(intensity), org_(org){}
 
     virtual Vector3 direction_from(const Vector3 &v) const = 0;
     Light_Intensity intensity() const;
+    virtual Vector3 position() const = 0;
 
 protected:
     Light_Intensity intensity_;
+    Vector3 org_;
 };
 
 class Point_Light: public Light
 {
 public:
-    Point_Light(const Light_Intensity &intensity, const Vector3 &org): Light(intensity), org_(org) {}
+    Point_Light(const Light_Intensity &intensity, const Vector3 &org): Light(intensity, org) {}
 
     Vector3 direction_from(const Vector3 &v) const override;
+    Vector3 position() const override;
 private:
-    Vector3 org_;
 };
