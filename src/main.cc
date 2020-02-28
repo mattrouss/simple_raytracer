4e8ac18af9fea45cafa0c970039994e87921ca12
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-02-28 09:09:14 +0100

    Add Spheres and planes to scene

--- src/main.cc
+++ src/main.cc
@@ -17,16 +17,23 @@ int main(int argc, char* argv[])
 
     Scene s(c, 1024, 1024);
 
-    Uniform_Texture ut_pink(0.3, 0.6, 8, 0.1, Color(255, 0, 150));
+    Uniform_Texture ut_pink(0.3, 0.6, 5, 0.1, Color(255, 0, 150));
+    Uniform_Texture ut_red(0.7, 0.2, 3, 0.05, Color(200, 0, 0));
+    Uniform_Texture ut_yellow(0.4, 0.4, 6, 0.5, Color(200, 200, 0));
     Uniform_Texture ut_green(0.5, 0.4, 5, 0.1, Color(0, 200, 0));
     Uniform_Texture ut_blue(0.5, 0.4, 4, 0.1, Color(0, 0, 200));
-    Uniform_Texture ut_gray(0.7, 0, 6, 0.2, Color(100, 100, 100));
+    Uniform_Texture ut_gray(0.7, 0.5, 8, 0.2, Color(100, 100, 100));
     s.add_object(new Plane({0, -5, 0}, {0, 1, 0}, ut_gray));
+    s.add_object(new Plane({-15, 0, 0}, {1, 0, 0}, ut_blue));
+    s.add_object(new Plane({15, 0, 0}, {-1, 0, 0}, ut_pink));
+    s.add_object(new Plane({0, 0, 40}, {0, 0, -1}, ut_gray));
+    s.add_object(new Sphere(3, {4, -1, 17}, ut_red));
+    s.add_object(new Sphere(1, {-5, 5, 15}, ut_gray));
     s.add_object(new Sphere(1, {4, -3, 10}, ut_blue));
     s.add_object(new Sphere(2, {0, -3, 14}, ut_pink));
     s.add_object(new Sphere(2, {-5, -3, 10}, ut_green));
     s.add_light_source(new Point_Light(50.f, Color_Intensity(1, 1, 1), Vector3(0, 20, 0)));
-    s.add_light_source(new Point_Light(20.f, Color_Intensity(1, 1, 1), Vector3(20, 10, 0)));
+    s.add_light_source(new Point_Light(20.f, Color_Intensity(1, 1, 1), Vector3(10, 3, 3)));
 
     auto start = std::chrono::high_resolution_clock::now();
     Image im = s.gen_img();
